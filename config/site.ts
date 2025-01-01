64a161b8c6785c4e19a7a0d223ca33f8a53cce53
Shivesh Anand <btech10218.21@bitmesra.ac.in>
2025-01-01 19:19:33 +0530

    Email changes

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Shivesh Anand",
   description: "Shivesh Anand Portfolio Website",
-  email: "theshiveshanand@gmail.com",
+  email: "theshiveshanand.official@gmail.com",
   images: {
     college:
       "https://res.cloudinary.com/dxiy3slnd/image/upload/v1735382777/bitmesra_fcz1t7.png",
